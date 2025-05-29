741bede174702f6e863ee6c982eb98f74cf43613
Muhammad Shaheer <muhammadshaheer4636@gmail.com>
2025-05-29 08:55:33 -0700

    Update window.tf

--- Terraform/ec2/window.tf
+++ Terraform/ec2/window.tf
@@ -4,12 +4,29 @@ provider "aws" {
 
 resource "aws_instance" "windows_server" {
   ami                    = "ami-070f90bab72874c6f"
-  instance_type          = "t2.large"  # You can change to your desired instance type
+  instance_type          = "t2.large"
   subnet_id              = "subnet-379e115c"
   vpc_security_group_ids = ["sg-0101e8fd37928991e"]
   iam_instance_profile   = "AmazonSSMRoleForInstancesQuickSetup"
-  key_name               = "lm-related-tee" 
+  key_name               = "lm-related-tee"  # Replace with your EC2 key pair if using RDP
+
+  user_data = <<-EOF
+    <powershell>
+    try {
+      $chromeUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
+      $installerPath = "$env:TEMP\\chrome_installer.exe"
+
+      Invoke-WebRequest -Uri $chromeUrl -OutFile $installerPath -UseBasicParsing
+      Start-Process -FilePath $installerPath -ArgumentList "/silent /install" -Wait
+      Remove-Item $installerPath -Force
+      Write-Output "Google Chrome installed successfully."
+    } catch {
+      Write-Error "Failed to install Chrome: $_"
+    }
+    </powershell>
+  EOF
+
   tags = {
-    Name = "WindowsServer"
+    Name = "Astadia-TEE"
   }
 }
