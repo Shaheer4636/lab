c8ac24c518e0c7a44d3435c1d333648d785110cf
Muhammad Shaheer <muhammadshaheer4636@gmail.com>
2025-06-02 06:39:15 -0700

    update main.tf using exisitng subnet group

--- Terraform/postgres-engine/main.tf
+++ Terraform/postgres-engine/main.tf
@@ -2,36 +2,24 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_db_subnet_group" "aurora_subnet_group" {
-  name       = "ak-test-public-db-lm-subnet-group"
-  subnet_ids = [
-    "subnet-06ff4248eaae65380",
-    "subnet-005b294f31a7f312d6"
-  ]
-
-  tags = {
-    Name    = "aurora-subnet-group"
-    project = "5-apps"
-    request = "AK"
-    managed = "Ayo"
-  }
+locals {
+  db_names = ["dsna", "dsnc", "dsnw"]
 }
 
 resource "aws_rds_cluster" "aurora_postgres" {
-  count                  = 3
-  cluster_identifier     = "aurora-cluster-${element(["dsna", "dsnc", "dsnw"], count.index)}"
+  count                  = length(local.db_names)
+  cluster_identifier     = "aurora-cluster-${local.db_names[count.index]}"
   engine                 = "aurora-postgresql"
   engine_version         = "16.8"
   master_username        = "admin"
   master_password        = "SuperSecurePassword123!"
-  database_name          = upper(element(["dsna", "dsnc", "dsnw"], count.index))
-  db_subnet_group_name   = aws_db_subnet_group.aurora_subnet_group.name
+  database_name          = upper(local.db_names[count.index])
+  db_subnet_group_name   = "ak-test-public-db-lm-subnet-group"
   vpc_security_group_ids = ["sg-0691fd3403a7e5577"]
   skip_final_snapshot    = true
-  publicly_accessible    = true
 
   tags = {
-    Name    = "aurora-${element(["dsna", "dsnc", "dsnw"], count.index)}"
+    Name    = "aurora-${local.db_names[count.index]}"
     project = "5-apps"
     request = "AK"
     managed = "Ayo"
@@ -39,15 +27,15 @@ resource "aws_rds_cluster" "aurora_postgres" {
 }
 
 resource "aws_rds_cluster_instance" "aurora_instance" {
-  count              = 3
-  identifier         = "aurora-instance-${element(["dsna", "dsnc", "dsnw"], count.index)}"
-  cluster_identifier = aws_rds_cluster.aurora_postgres[count.index].id
-  instance_class     = "db.t3.large"
-  engine             = "aurora-postgresql"
-  publicly_accessible = true
+  count                = length(local.db_names)
+  identifier           = "aurora-instance-${local.db_names[count.index]}"
+  cluster_identifier   = aws_rds_cluster.aurora_postgres[count.index].id
+  instance_class       = "db.t3.large"
+  engine               = "aurora-postgresql"
+  publicly_accessible  = true
 
   tags = {
-    Name    = "aurora-instance-${element(["dsna", "dsnc", "dsnw"], count.index)}"
+    Name    = "aurora-instance-${local.db_names[count.index]}"
     project = "5-apps"
     request = "AK"
     managed = "Ayo"
