525e4a1b2ce18886c9e3682f384b79b9f1087aea
Muhammad Shaheer <muhammadshaheer4636@gmail.com>
2025-07-14 13:15:45 -0500

    Update resource-tagging-aws.sh

--- resource-tagging-aws.sh
+++ resource-tagging-aws.sh
@@ -1,35 +1,63 @@
-#!/bin/bash
+# Define the stage names
+$stageNames = @(
+    "$(HCC.QA.NewServices.Qa.StageName)",
+    "$(HCC.QA.NewServices.B2B.StageName)"
+)
 
-# Set your desired tag
-TAG_KEY="project"
-TAG_VALUE="liberty"
+# Var to track the overall status
+$allSucceededOrFailed = $true
 
-echo "[*] Fetching all AWS regions..."
-REGIONS=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
+# Loop through stages
+foreach ($stageName in $stageNames) {
+    # Construct environment variable name
+    $envVarName = "RELEASE_ENVIRONMENTS_${stageName}_STATUS"
 
-# Loop through all regions
-for REGION in $REGIONS; do
-  echo -e "\n[+] Processing region: $REGION"
-  
-  # Get all resource ARNs in this region
-  RESOURCE_ARNS=$(aws resourcegroupstaggingapi get-resources \
-    --region "$REGION" \
-    --query "ResourceTagMappingList[].ResourceARN" \
-    --output text)
+    echo $envVarName
 
-  if [ -z "$RESOURCE_ARNS" ]; then
-    echo "[-] No resources found in $REGION"
-    continue
-  fi
+    # Retrieve status from pipeline
+    $status = Get-Content env:$envVarName
 
-  for ARN in $RESOURCE_ARNS; do
-    echo "[+] Tagging $ARN with $TAG_KEY=$TAG_VALUE"
-    aws resourcegroupstaggingapi tag-resources \
-      --region "$REGION" \
-      --resource-arn-list "$ARN" \
-      --tags $TAG_KEY=$TAG_VALUE \
-      --output text
-  done
-done
+    # Output the status for debugging purposes
+    Write-Output "Status of ${stageName}: ${status}"
 
-echo -e "\n[✓] Tagging completed across all regions."
+    # Check if status is neither succeeded nor failed
+    if ($status -ne "succeeded" -and $status -ne "failed") {
+        $allSucceededOrFailed = $false
+        break
+    }
+}
+
+# Execute if all stages are either succeeded or failed
+if ($allSucceededOrFailed) {
+
+    Write-Output "All stages have either succeeded or failed. Copying files from fileshare..."
+    echo "echo $(System.DefaultWorkingDirectory)"
+    echo $(System.DefaultWorkingDirectory)
+
+    echo "set AZCOPY_LOG_LOCATION=$(System.DefaultWorkingDirectory)"
+    set AZCOPY_LOG_LOCATION=$(System.DefaultWorkingDirectory)
+
+    echo "set AZCOPY_AUTO_LOGIN_TYPE=MSI"
+    set AZCOPY_AUTO_LOGIN_TYPE=MSI
+
+    
+    echo "$(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\azcopy.exe login --login-type=MSI --log-level=DEBUG --tenant-id "$(ReleasePipeline.TenantID)""
+
+    $(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\azcopy.exe login --login-type=MSI --log-level=DEBUG --tenant-id "$(ReleasePipeline.TenantID)"
+
+    
+    echo "$(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\azcopy.exe copy "$(ReleasePipeline.StorageAccount)/$(ReleasePipeline.ReportFolder)/$(Release.DefinitionName)/$(releasenumber)/allure-results/*" "$(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\allure-results" --recursive --log-level=DEBUG"
+    
+    $(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\azcopy.exe copy "$(ReleasePipeline.StorageAccount)/$(ReleasePipeline.ReportFolder)/$(Release.DefinitionName)/$(releasenumber)/allure-results/*" "$(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\allure-results" --recursive --log-level=DEBUG
+
+
+
+echo "$(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\azcopy.exe copy "$(ReleasePipeline.StorageAccount)/$(ReleasePipeline.ReportFolder)/$(Release.DefinitionName)/$(releasenumber)/cypress/results/*" "$(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\cypress\results" --recursive --log-level=DEBUG"
+    
+    $(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\azcopy.exe copy "$(ReleasePipeline.StorageAccount)/$(ReleasePipeline.ReportFolder)/$(Release.DefinitionName)/$(releasenumber)/cypress/results/*" "$(System.DefaultWorkingDirectory)\artifact\$(Build.BuildId)\cypress\results" --recursive --log-level=DEBUG
+
+
+    # Invoke-MyCommand
+} else {
+    Write-Output "One or more test stages have a status other than succeeded or failed."
+}
