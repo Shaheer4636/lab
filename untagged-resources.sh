cdb006bcc5af794861190efce56af6b3a346e37d
Muhammad Shaheer <muhammadshaheer4636@gmail.com>
2025-07-16 09:31:04 -0700

    Update untagged-resources.sh

--- untagged-resources.sh
+++ untagged-resources.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "========== Untagged AWS Resources =========="
-printf "%-12s | %s\n" "ResourceType" "ResourceIdentifier"
-printf -- "---------------------------------------------\n"
+echo -e "ResourceType\tResourceIdentifier"
+echo -e "------------\t------------------"
 
 # EC2 Instances
 for instance_id in $(aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" --output text); do
   tags=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" --query "Tags" --output json)
   if [ "$tags" == "[]" ]; then
-    printf "%-12s | %s\n" "EC2" "$instance_id"
+    echo -e "EC2\t\t$instance_id"
   fi
 done
 
@@ -17,15 +17,15 @@ for db in $(aws rds describe-db-instances --query "DBInstances[].DBInstanceIdent
   arn=$(aws rds describe-db-instances --db-instance-identifier "$db" --query "DBInstances[0].DBInstanceArn" --output text)
   tags=$(aws rds list-tags-for-resource --resource-name "$arn" --query "TagList" --output json)
   if [ "$tags" == "[]" ]; then
-    printf "%-12s | %s\n" "RDS" "$db"
+    echo -e "RDS\t\t$db"
   fi
 done
 
 # S3 Buckets
 for bucket in $(aws s3api list-buckets --query "Buckets[].Name" --output text); do
   tags=$(aws s3api get-bucket-tagging --bucket "$bucket" --query "TagSet" --output json 2>/dev/null)
-  if [ "$tags" == "" ]; then
-    printf "%-12s | %s\n" "S3" "$bucket"
+  if [ -z "$tags" ] || [ "$tags" == "[]" ]; then
+    echo -e "S3\t\t$bucket"
   fi
 done
 
@@ -34,6 +34,6 @@ for func in $(aws lambda list-functions --query "Functions[].FunctionName" --out
   arn=$(aws lambda get-function --function-name "$func" --query "Configuration.FunctionArn" --output text)
   tags=$(aws lambda list-tags --resource "$arn" --query "Tags" --output json)
   if [ "$tags" == "{}" ]; then
-    printf "%-12s | %s\n" "Lambda" "$func"
+    echo -e "Lambda\t\t$func"
   fi
 done
