8cf7c57033f516af50d235e70ffb9f30002c96ca
Muhammad Shaheer <muhammadshaheer4636@gmail.com>
2025-07-23 13:14:09 -0500

    Update user-sudo-access.sh

--- user-sudo-access.sh
+++ user-sudo-access.sh
@@ -1,29 +1,8 @@
-#!/bin/bash
-
-# Create the user and set password
-useradd -m -s /bin/bash rahul
-echo "rahul:rahul@liberty123!" | chpasswd
-
-# Add user to sudo group
-usermod -aG sudo rahul
-
-# Set up SSH folder and permissions
-mkdir -p /home/rahul/.ssh
-chmod 700 /home/rahul/.ssh
-touch /home/rahul/.ssh/authorized_keys
-chmod 600 /home/rahul/.ssh/authorized_keys
-chown -R rahul:rahul /home/rahul/.ssh
-
-# Optional: Copy authorized_keys from current user (if needed)
-# cp ~/.ssh/authorized_keys /home/rahul/.ssh/authorized_keys
-# chown rahul:rahul /home/rahul/.ssh/authorized_keys
-
-# Ensure SSH is enabled
-systemctl enable ssh
-systemctl start ssh
-
-# Set full permissions on home directory
-chmod 700 /home/rahul
-chown -R rahul:rahul /home/rahul
-
-echo "✅ User 'rahul' created with sudo and SSH access."
+az artifacts universal download \
+--organization "https://dev.azure.com/astadia/" \
+--project "56fdb26f-df8a-4616-9c92-a92ed87ffb0d" \
+--scope project \
+--feed "dowitcher-full" \
+--name "dowitcher" \
+--version "1.1.14" \
+--path .
