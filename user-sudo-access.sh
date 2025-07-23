1ff9f68748bb1f52f622b5ac94f7d6234dd5a085
Muhammad Shaheer <muhammadshaheer4636@gmail.com>
2025-07-23 12:12:18 -0700

    Update user-sudo-access.sh

--- user-sudo-access.sh
+++ user-sudo-access.sh
@@ -1,12 +1,1 @@
-az artifacts universal download `
-  --organization "https://dev.azure.com/astadia/" `
-  --project "56fdb26f-df8a-4616-9c92-a92ed87ffb0d" `
-  --scope project `
-  --feed "dowitcher-full" `
-  --name "dowitcher" `
-  --version "1.1.14" `
-  --path .
-
-
-
-BN6rOwW3wXqCfYd0HLkt0PcFxSh5cFdh4eiTxvtkEiGwbNO9oamSJQQJ99BGACAAAAA549A1AAASAZDO3sqr
+hhdhsd
