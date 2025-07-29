c9e591752adc0ce0438d5f9da9115891f54fceee
AyyoShaheer DevOps <muhammadshaheer4636@gmail.com>
2025-07-29 10:29:20 -0700

    Update user-sudo-access.sh

--- user-sudo-access.sh
+++ user-sudo-access.sh
@@ -1,3 +1,1 @@
-Hey, the file taapmtin.txt is in /home/ec2-user, not in the LIBRARY/ directory.
-That’s why the rename is failing — it’s looking in the wrong path.
-Update the script to rename the file directly in the current directory.
+robotos.xt
