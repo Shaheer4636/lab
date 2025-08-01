466fa0e8eeede65781107dc9a3b20654a3c33b3e
AyyoShaheer DevOps <muhammadshaheer4636@gmail.com>
2025-08-01 13:55:02 -0700

    Update user-sudo-access.sh

--- user-sudo-access.sh
+++ user-sudo-access.sh
@@ -1,15 +1,80 @@
-# Enable multiple simultaneous RDP sessions
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name 'fSingleSessionPerUser' -Value 0
+# ------------------ SET FIXED CONFIG ------------------
+RESOURCE_GROUP="vm-rdp-test"
+LOCATION="eastus"
+VNET_NAME="vm-rdp-vnet"
+VM_SUBNET_NAME="vm-subnet"
+BASTION_SUBNET_NAME="AzureBastionSubnet"
+VM_NAME="win2019-rdpvm"
+BASTION_NAME="bastion-host"
+PUBLIC_IP_NAME="bastion-pip"
+ADMIN_USERNAME="adminuser"
+ADMIN_PASSWORD="P@ssw0rd1234!"
+VM_SIZE="Standard_D2s_v3"
+DISK_SIZE=64
 
-# Optional: Increase max concurrent RDP connections (uncomment if needed)
-# Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'MaxInstanceCount' -Value 10
+# ------------------ SET SUBSCRIPTION ------------------
+az account set --subscription "Astadia Dev"
 
-# Create Users with Passwords
+# ------------------ CREATE RESOURCE GROUP ------------------
+az group create \
+  --name $RESOURCE_GROUP \
+  --location $LOCATION
+
+# ------------------ CREATE VNET AND SUBNETS ------------------
+az network vnet create \
+  --resource-group $RESOURCE_GROUP \
+  --location $LOCATION \
+  --name $VNET_NAME \
+  --address-prefix 10.0.0.0/16 \
+  --subnet-name $VM_SUBNET_NAME \
+  --subnet-prefix 10.0.1.0/24
+
+az network vnet subnet create \
+  --resource-group $RESOURCE_GROUP \
+  --vnet-name $VNET_NAME \
+  --name $BASTION_SUBNET_NAME \
+  --address-prefix 10.0.2.0/27
+
+# ------------------ CREATE PUBLIC IP FOR BASTION ------------------
+az network public-ip create \
+  --resource-group $RESOURCE_GROUP \
+  --name $PUBLIC_IP_NAME \
+  --sku Standard \
+  --location $LOCATION
+
+# ------------------ CREATE BASTION HOST ------------------
+az network bastion create \
+  --name $BASTION_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --vnet-name $VNET_NAME \
+  --location $LOCATION \
+  --public-ip-address $PUBLIC_IP_NAME \
+  --sku Basic
+
+# ------------------ CREATE WINDOWS SERVER 2019 VM ------------------
+az vm create \
+  --resource-group $RESOURCE_GROUP \
+  --name $VM_NAME \
+  --image "MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest" \
+  --size $VM_SIZE \
+  --admin-username $ADMIN_USERNAME \
+  --admin-password $ADMIN_PASSWORD \
+  --vnet-name $VNET_NAME \
+  --subnet $VM_SUBNET_NAME \
+  --os-disk-size-gb $DISK_SIZE \
+  --nsg "" \
+  --public-ip-address "" \
+  --license-type Windows_Server \
+  --location $LOCATION
+
+# ------------------ CREATE 2 ADDITIONAL USERS ------------------
+az vm run-command invoke \
+  --resource-group $RESOURCE_GROUP \
+  --name $VM_NAME \
+  --command-id RunPowerShellScript \
+  --scripts @- <<'EOF'
 net user user1 "UserP@ssw0rd1!" /add
 net user user2 "UserP@ssw0rd2!" /add
-net user user3 "UserP@ssw0rd3!" /add
-
-# Add users to Remote Desktop Users group
 net localgroup "Remote Desktop Users" user1 /add
 net localgroup "Remote Desktop Users" user2 /add
-net localgroup "Remote Desktop Users" user3 /add
+EOF
