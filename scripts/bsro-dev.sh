21be04706e58f53739bceee3971e06fb6065ce9c
AyyoShaheer DevOps <muhammadshaheer4636@gmail.com>
2025-08-27 09:54:36 -0700

    Update bsro-dev.sh

--- scripts/bsro-dev.sh
+++ scripts/bsro-dev.sh
@@ -1,16 +1,4 @@
-set -euo pipefail
-
-# Bind ADO variables
-SRC='$(SOURCE_DIR)/'
-DST='$(PIPELINE_USER)@$(SERVER_IP):$(TARGET_DIR)/'
-KEY_FILE='$(KEY.secureFilePath)'
-
-chmod 600 "$KEY_FILE"
-
-echo "SRC: $SRC"
-echo "DST: $DST"
-
-# Use the key via ssh
-rsync -rltDv --info=progress2 \
-  -e "ssh -i $KEY_FILE -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
-  "$SRC" "$DST"
+TARGET_DIR='$(TARGET_DIR)'; APP_USER='$(APP_USER)'; PIPELINE_USER='$(PIPELINE_USER)';
+sudo mkdir -p "$TARGET_DIR";
+sudo chown -R "$PIPELINE_USER":"$APP_USER" "$TARGET_DIR";
+sudo chmod -R g+rwX "$TARGET_DIR"
